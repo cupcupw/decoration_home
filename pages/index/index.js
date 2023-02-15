a1b59ebb3e564951944b5b68666795fd2864f8ec
wanghaibo <1056140533@qq.com>
2023-02-15 13:17:22 +0800

    chore: 暂存

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,23 @@
 Page({
   data: {
+    typeList:[]
   },
   onLoad() {
+    const _this = this
+    wx.request({
+      url: 'http://localhost:3000/queryRenovation',
+      success(res){
+        _this.setData({
+          typeList:res.data
+        })
+        console.log(_this.data.typeList);
+      }
+    })
   },
+  handlejump(e){
+    const { id } = e.currentTarget.dataset
+    wx.navigateTo({
+      url: `/pages/decoration_detail/index?id=${id}`,
+    })
+  }
 })
